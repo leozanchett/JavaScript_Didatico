d22d4c4cc47106626a07885c6296cb5d4cb2d7b5
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-21 16:07:29 -0300

    setTimeout()
    Exemplo simples.

--- Promises/app.js
+++ Promises/app.js
@@ -1,3 +1,5 @@
+// PROMISE
+
 const inventory = {
     sunglasses: 1900,
     pants: 1088,
@@ -16,3 +18,16 @@ if(inventory.sunglasses > 0){
 const orderSunglasses = () => new Promise(myExecutor);
 const orderPromise = orderSunglasses();
 console.log(orderPromise);
+
+
+
+// setTimeout()
+
+console.log("This is the first line of code in app.js.");
+const usingSTO = () => {
+  console.log('what');
+}
+
+setTimeout(usingSTO, 2000);
+
+console.log("This is the last line of code in app.js.");
