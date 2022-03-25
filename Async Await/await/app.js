f24131186cd1e9d96df5cde4355366c166401134
Leo <45577227+leozanchett@users.noreply.github.com>
2022-03-24 21:25:29 -0300

    await com then e catch

--- Async Await/await/app.js
+++ Async Await/await/app.js
@@ -11,8 +11,12 @@ function nativePromiseDinner() {
 
 // async/await version:
 async function announceDinner() {
-    const meal = await brainstormDinner();
-    console.log(`I'm going to make ${meal} for dinner.`);  
+    //const meal = await brainstormDinner();
+    await brainstormDinner().then((meal) => {
+        console.log(`I'm going to make ${meal} for dinner.`);  
+    }).catch((erro ) => {
+        console.log(`deu ruim em ${erro}`);
+    });
 }
 
 //nativePromiseDinner();
