138fe54cb487e536b4b40cdd7ac93cf4c975b4be
Leo <45577227+leozanchett@users.noreply.github.com>
2022-06-29 11:04:30 -0300

    Update basics.js

--- FREE code camp/basics.js
+++ FREE code camp/basics.js
@@ -16,3 +16,46 @@ console.log(sampleStr);
 
 // array multidimensional
 const teams = [["Bulls", 23], ["White Sox", 45]];
+
+
+const myArray = [["John", 23], ["cat", 2]];
+
+let removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray);
+
+
+const myList = [];
+myList.unshift(["Chocolate", 35]);
+myList.unshift(["Coffee", 25]);
+myList.unshift(["Milk", 15]);
+myList.unshift(["Bread", 10]);
+myList.unshift(["Eggs", 5]);
+console.log(myList);
+
+
+function trueOrFalse(wasThatTrue) {
+    return wasThatTrue ? "Yes, that was true" : "No, that was false";
+    //return wasThatTrue && "Yes, that was true" || "No, that was false";
+}
+
+console.log(trueOrFalse(true));
+
+
+typeof 3; // number
+typeof "3"; // string
+typeof true; // boolean
+typeof undefined; // undefined
+typeof null; // object
+typeof {}; // object
+typeof []; // array
+typeof function () {}; // function
+typeof Symbol(); // symbol
+typeof new Date(); // object
+typeof new Error(); // object
+
+
+1 != 2; // true
+1 == 2; // false
+1 === 2; // false
+1 !== 2; // true
+
